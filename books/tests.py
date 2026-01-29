9168ee097d3f7158747c2fcf7595b7da1026c21c
osama <osaseid1@gmail.com>
2026-01-29 22:57:09 +0300

    test

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Book
 
-# Create your tests here.
+class BookModelTest(TestCase):
+
+    def setUp(self):
+        # Create a sample book
+        Book.objects.create(title="Test Book", author="Osama", published_year=2024)
+
+    def test_book_creation(self):
+        """Test if book is created correctly"""
+        book = Book.objects.get(title="Test Book")
+        self.assertEqual(book.author, "Osama")
+
+    def test_book_count(self):
+        """Test number of books"""
+        count = Book.objects.count()
+        self.assertEqual(count, 1)
